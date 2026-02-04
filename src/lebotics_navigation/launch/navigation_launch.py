e1787138fb2c14bdb1dda42d030bce9f46d26833
TalaChehade04 <shehadehtala0@gmail.com>
2026-02-04 13:53:58 +0200

    Update navigation_launch.py

--- src/lebotics_navigation/launch/navigation_launch.py
+++ src/lebotics_navigation/launch/navigation_launch.py
@@ -1,16 +1,18 @@
 import os
-from celebrated_package_path import get_package_share_directory # Helper
+from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription, SetEnvironmentVariable
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.actions import Node
 
 def generate_launch_description():
-    # 1. Paths to the files
+    # 1. Paths to your files
+    # Make sure this matches the project name in your CMakeLists.txt
     pkg_dir = get_package_share_directory('lebotics_navigation')
     map_file = os.path.join(pkg_dir, 'maps', 'my_map.yaml')
     
     # 2. Path to standard TurtleBot3 & Nav2 launches
+    # These packages must be installed via sudo apt install
     gazebo_launch = IncludeLaunchDescription(
         PythonLaunchDescriptionSource([
             os.path.join(get_package_share_directory('turtlebot3_gazebo'), 'launch', 'turtlebot3_world.launch.py')
